8379e601cc0d06319edcf07210432c987f371b63
Spencer Curtis <spencerscurtis@gmail.com>
2021-03-04 18:38:34 -0700

    sub full crud

---
+++ app/javascript/components/Subs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const url = "http://localhost:3000/subs/";
+
+const Subs = ({ subs }) => {
+  return (
+    <>
+      <h1>All the Subs</h1>
+      <a href={"/subs/new"}>Add Sub</a>
+      { subs.map( (sub) =>
+        <div>
+          <h3>{sub.title}</h3>
+          <a href={url + sub.id}>Show</a>
+          <br />
+          <a href={url + sub.id + "/edit"}>Edit</a>
+          <br />
+          <a
+            href={url + sub.id }
+            data-method="delete"
+          >
+            Delete
+          </a>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Subs;
--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
   resources :subs do
     resources :topics
   end
-  resources :topics do
-    resources :comments
-  end
+
+  # resources :topics do
+  #   resources :comments
+  # end
+
   # resources :comments do
   #   resources :likes
   # end
